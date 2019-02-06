5c1cf4ef81e75aa51b33a95c922a6a9ecdfe8cf6
vagrant <vagrant@homestead>
2019-02-06 15:33:33 +0000

    update

--- resources/views/kexiaoqi/result.blade.php
+++ resources/views/kexiaoqi/result.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.kexiaoqiDefault')
 @section('content')
-  <h3>他想对你说 ：晚安，笨蛋 , 什么时候回来鸭<h3>
+  <h3>他想对你说 ：我想我是是太无聊了 ，玩的开心猪 ，想 ， 可以在这留言给我<h3>
   <h4>你想对他说什么 ：</h4>
   		@include('kexiaoqi._error')
         <form method="POST" action="{{ route('kexiaoqiEcho') }}">
